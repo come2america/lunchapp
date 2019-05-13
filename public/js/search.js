885e9bdead2e6cbcc6a1b0f87a607b551c24da88
Yusuf <ysserunjogi@gmail.com>
2019-05-13 06:58:59 -0700

    nutritiondata3

--- public/js/search.js
+++ public/js/search.js
@@ -496,7 +496,20 @@ $(document).ready(function () {
             throw err;
         });
     });
-
+    $(document).ready(function () {
+        $(".popup").hide();
+        $(".openpop").click(function (e) {
+            e.preventDefault();
+            $("iframe").attr("src", $(this).attr('href'));
+            $(".links").fadeOut('slow');
+            $(".popup").fadeIn('slow');
+        });
+    
+        $(".close").click(function () {
+            $(this).parent().fadeOut("slow");
+            $(".links").fadeIn("slow");
+        });
+    });
 
 });
 
