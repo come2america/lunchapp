c7f96d6b0129e3ddf7c9202dd2f2c7e7b06ddcd3
Yusuf <ysserunjogi@gmail.com>
2019-05-19 07:39:21 -0700

    request lunch

--- models/lunch.model.js
+++ models/lunch.model.js
@@ -4,7 +4,9 @@ module.exports = function (sequelize, DataTypes) {
         eater: DataTypes.STRING,
         lunch: DataTypes.STRING,
         ingredients: DataTypes.STRING,
+        school: DataTypes.STRING,
         tradable: DataTypes.BOOLEAN,
+        requested: DataTypes.BOOLEAN,
     });
     return Lunch;
 };
--- public/js/search.js
+++ public/js/search.js
@@ -138,6 +138,7 @@ $(document).ready(function () {
                         headers: { "Authorization": 'Bearer ' + token }
                     }).then(function (resp) {
                         $(".modal-trade").html("<h4>Will you trade your [" + food + "]<br>with [" + resp.lunch + "]?</h4>");
+                        $(".modal-trade").html("<h4>Will you send me [" + food + "]<br>as requested [" + resp.lunch + "]?</h4>");
                         $("#modal2").modal("open");
                     }).catch(function (err) {
                         throw err;
@@ -215,9 +216,14 @@ $(document).ready(function () {
                     for (var i = 0; i < resp.length; i++) {
                         var food = resp[i].lunch + "<span style='font-size:14px; font-weight:100; font-style: italic;'>("+resp[i].ingredients+")</span>" +
                         // var ingredients = ;
-                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].createdAt + ")</span>"
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].createdAt + ")</span>" +
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].school + ")</span>"
+                       
                         if (resp[i].tradable) {
                                 food += " <span style='font-size:14px; font-weight:100; font-style: italic;'>(tradable)</span>";
+                            }
+                          if(resp[i].requested ) {
+                                food += " <span style='font-size:14px; font-weight:100; font-style: italic;'>(requested)</span>";
                             };
                         var foodName = $("<div class='myFoodName col s10 amber lighten-4'>").attr("id", "food" + resp[i].id).html(food);
                         
@@ -244,12 +250,14 @@ $(document).ready(function () {
                 var counter = 0;
                 $("#searchItems").empty();
                 for (var i = 0; i < resp.length; i++) {
-                    if (payload.userID != resp[i].userID && resp[i].tradable) {
+                    if (payload.userID != resp[i].userID && resp[i].tradable||resp[i].requested) {
                         counter++;
                         var food = resp[i].lunch + "<span style='font-size:14px; font-weight:100; font-style: italic;'>("+resp[i].ingredients+")</span>" +
                         // var ingredients = ;
-                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].createdAt + ")</span>"
-                        if (resp[i].tradable) {
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].createdAt + ")</span>"+
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].eater + ")</span>"+
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].school + ")</span>"
+                        if (resp[i].tradable ||resp[i].requested ) {
                                 food += " <span style='font-size:14px; font-weight:100; font-style: italic;'>(tradable)</span>";
                             };
                         var foodName = $("<div class='myFoodName col s10 amber lighten-4'>").attr("id", "food" + resp[i].id).html(food);
@@ -268,16 +276,60 @@ $(document).ready(function () {
                 throw err;
             });
     };
+    function requestedLunch() {
+        $.ajax({
+            url: "/api/lunch",
+            method: "GET",
+            headers: { "Authorization": 'Bearer ' + token }
+        })
+            .then(function (resp) {
+                var counter = 0;
+                $("#requestedItems").empty();
+                for (var i = 0; i < resp.length; i++) {
+                    if (payload.userID != resp[i].userID && resp[i].requested) {
+                        counter++;
+                        var food = resp[i].lunch + "<span style='font-size:14px; font-weight:100; font-style: italic;'>("+resp[i].ingredients+")</span>" +
+                        // var ingredients = ;
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].createdAt + ")</span>"+
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].eater + ")</span>"+
+                        "<span style='font-size:14px; font-weight:100; font-style: italic;'> (" + resp[i].school + ")</span>"
+                        if (resp[i].requested ) {
+                                food += " <span style='font-size:14px; font-weight:100; font-style: italic;'>(requested)</span>";
+                            };
+                        var foodName = $("<div class='myFoodName col s10 amber lighten-4'>").attr("id", "food" + resp[i].id).html(food);
+                        
+                        var button = $('<button class="foodTrade waves-effect waves-light btn orange lighten-1">').attr({ "id": resp[i].id, "data-name": resp[i].userID }).text("Send");
+                        var foodOption = $("<div class='foodOption col s3'>").append(button);
+                        $("#requestedItems").append(foodName).append(foodOption);
+                        
 
+                    };
+                };
+                if (counter === 0) {
+                    $("#requestedItems").html("<h4>Nothing...</h4>");
+                };
+            }).catch(function (err) {
+                throw err;
+            });
+    };
     $("#allTradable1").on("click", function (event) {
         event.preventDefault();
         tradableLunch();
+    });
+    $("#allrequested1").on("click", function (event) {
+        event.preventDefault();
+        requestedLunch();
     });
 
     $("#allTradable2").on("click", function (event) {
         event.preventDefault();
         $('.sidenav').sidenav("close");
         tradableLunch();
+    });
+    $("#allrequested2").on("click", function (event) {
+        event.preventDefault();
+        $('.sidenav').sidenav("close");
+       requestedLunch();
     });
 
     $("#signOut1").on("click", function (event) {
@@ -307,8 +359,10 @@ $(document).ready(function () {
                 "userID": payload.userID,
                 "eater": payload.name,
                 "lunch": $("#foodName").val(),
-                "ingredients": $("#ingredients").val(),              
-               "tradable": $("#trade").is(":checked")
+                "ingredients": $("#ingredients").val(), 
+                "school": $("#school").val(),       
+               "tradable": $("#trade").is(":checked"),
+               "requested": $("#requested").is(":checked")
             }
         }).then(function (resp) {
             myLunch();
@@ -414,6 +468,36 @@ $(document).ready(function () {
             }, 800);
         };
     });
+    $(document).on("click", ".foodsend", function (event) {
+        event.preventDefault();
+
+        yourID = Number($(this).attr("data-name"));
+        yourFood = Number($(this).attr("id"));
+        $(".foodName").removeClass("darken-2");
+        $("#food" + $(this).attr("id")).addClass("darken-2");
+
+        $("#myLunches").css({ "background-color": "rgba(139, 2, 2, 0.8)" });
+        $("#myTitle").css({ "background-color": "white", "color": "rgb(139, 2, 2)" }).text("SELECT YOUR FOOD TO BE TRADED");
+
+        $("#myItems").empty();
+
+        var counter = 0;
+        for (var i = 0; i < lunchList.length; i++) {
+            if (lunchList[i].requested) {
+                counter++;
+                var foodName = $("<div class='myFoodName col s10 amber lighten-4'>").attr({ "id": "food" + lunchList[i].id, "value": lunchList[i].id }).html(lunchList[i].lunch);
+                var button = $('<button class="mysend waves-effect waves-light btn orange lighten-1">').attr("id", lunchList[i].id).html('<i class="material-icons prefix">autorenew</i>');
+                var foodOption = $("<div class='foodOption col s2'>").append(button);
+                $("#myItems").append(foodName).append(foodOption);
+            }
+        };
+        if (counter === 0) {
+            $("#myItems").html("<h4>Nothing to send...</h4>");
+            window.setTimeout(function () {
+                window.location.assign("/search.html")
+            }, 800);
+        };
+    });
 
     $(document).on("click", ".myTrade", function (event) {
         event.preventDefault();
@@ -421,7 +505,32 @@ $(document).ready(function () {
         myFood = Number($(this).attr("id"));
         $("#food" + $(this).attr("id")).addClass("darken-2");
 
-        $(".modal-content").html("<h4>A REQUEST HAS BEEN SENT!</h4>")
+        $(".modal-content").html("<h4>A REQUEST HAS BEEN SENT(Check Ingredients For Allergens)!</h4>")
+        $("#modal1").modal("open");
+
+        reqList.push(yourID);
+        mine.push(yourFood);
+        yours.push(myFood);
+
+        database.ref().set({
+            accepts: accepts,
+            rejects: rejects,
+            reqList: reqList,
+            mine: mine,
+            yours: yours
+        });
+
+        window.setTimeout(function () {
+            window.location.assign("/search.html")
+        }, 2000);
+    });
+    $(document).on("click", ".mysend", function (event) {
+        event.preventDefault();
+
+        myFood = Number($(this).attr("id"));
+        $("#food" + $(this).attr("id")).addClass("darken-2");
+
+        $(".modal-content").html("<h4>A FOOD REQUEST HAS BEEN SENT(Check Ingredients For Allergens)!</h4>")
         $("#modal1").modal("open");
 
         reqList.push(yourID);
@@ -471,6 +580,12 @@ $(document).ready(function () {
         });
     };
 
+
+    $(document).on("click", "#phonenumber", function (event) {
+        event.preventDefault();
+        $(".modal-content").html("<h4>Text 702-781-0949 about your request!</h4>")
+        $("#modal1").modal("open");
+    })
     $(document).on("click", "#agree", function (event) {
         event.preventDefault();
 
@@ -494,7 +609,7 @@ $(document).ready(function () {
                 update(yours[position], my.userID, my.eater);
                 update(mine[position], your.userID, your.eater);
 
-                $(".modal-content").html("<h4>A TRADE HAS BEEN COMPLETED!</h4>")
+                $(".modal-content").html("<h4>A TRADE HAS BEEN COMPLETED(Check Ingredients For Allergens)!</h4>")
                 $("#modal1").modal("open");
         
                 reqList.splice(position, 1);
@@ -551,13 +666,13 @@ $(document).ready(function () {
         });
     });
   
-    function onMessageAdded(data) {
-        let template = $("#new-message").html();
-        template = template.replace(req.body, data.message);
+    // function onMessageAdded(data) {
+    //     let template = $("#new-message").html();
+    //     template = template.replace(req.body, data.message);
       
 
-        $(".chat").append(template);
-    }
+    //     $(".chat").append(template);
+    // }
 
     // if(!isAuthenticated && !window.location.hash){
     //     lock.show();
